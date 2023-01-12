5d6135f709cc10585d4cdeae7524e2a3946d1e45
heewon Hwang <huiwon1488@gmail.com>
2023-01-12 12:23:58 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+// App.js
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import Lifecycle from "./Lifecycle";
+
+function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+  return (
+    <div className="App">
+      <Lifecycle />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
+    </div>
+  );
+}
+
+export default App;
